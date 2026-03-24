0a3f6ca05144577b8debc7fdde545d70977c34b6
Saif-Uddin0 <mahirr.846@gmail.com>
2026-03-25 04:19:56 +0600

    gnsdf

--- socket/orderHandler.js
+++ socket/orderHandler.js
@@ -1,15 +1,15 @@
 import { getCollection } from "../config/database.js";
 import { calculateTotal, createOrderDocument, orderIdGenerator } from "../utilities/helper.js";
 
-export const orderHandler =(io, socket)=>{
-    console.log("a user is connected" , socket.id);
+export const orderHandler = (io, socket) => {
+    console.log("a user is connected", socket.id);
 
     // place a order
-    socket.on("placeOrder", async(data , callback)=>{
-        try{
+    socket.on("placeOrder", async (data, callback) => {
+        try {
             console.log(`place order from ${socket.id}`)
             const validation = validateOrder(data);
-            if(!validation.valid){
+            if (!validation.valid) {
                 return callback({
                     success: false,
                     message: validation.message
@@ -17,23 +17,22 @@ export const orderHandler =(io, socket)=>{
             }
             const totals = calculateTotal(data.items);
             const orderId = orderIdGenerator();
-            const order = createOrderDocument(data, orderId , totals);
+            const order = createOrderDocument(data, orderId, totals);
             const ordersCollection = getCollection("orders");
             await ordersCollection.insertOne(order);
 
-             socket.join(`order-${orderId}`);
-             socket.join(`customers'`);
+            socket.join(`order-${orderId}`);
+            socket.join(`customers`);
 
-             io.to('admins').emit('newOrder' , {order})
+            io.to('admins').emit('newOrder', { order });
 
-             callback({success: true , order})
-             console.log(`order created: ${orderId}`);
-             
-            
-        }catch(error){
+            callback({ success: true, order })
+            console.log(`order created: ${orderId}`);
+
+
+        } catch (error) {
             console.log(error);
-            callback({success: false , message: "Faileeed to place order"})
+            callback({ success: false, message: "Faileeed to place order" })
         }
-    })
-    
+    });
 }
